7630e136edbf17cc111443baf0c12331bc8feb52
Andrea Fiadone <andrea.fiadone@purplenetwork.it>
2017-06-22 00:37:37 +0200

    Impletata funzione filtro + mouse drag

--- example/index.js
+++ example/index.js
@@ -1,40 +1,76 @@
 require('../style.css')
 var Select = require('..')
 
-var el = document.getElementById('select')
+var daysEl = document.getElementById('days')
+var monthsEl = document.getElementById('months')
 
-var select = new Select(el, {
-  data: getData(1980)
-})
-select.value(1990)
 
-select.on('change', function (v) {
-  console.log(v)
+var days = new Select(daysEl, {
+	data: getDays()
 })
+days.value(15)
+
+var months = new Select(monthsEl, {
+  data: getMonths()
+})
+months.value(6)
+months.on('change', function(v) {
+	if (v == 2) {
+		days.filter([30, 31]);
+	} else {
+		days.filter();
+	}
+})
+
+// select.on('change', function (v) {
+//   console.log(v)
+// })
 
 function getData(from) {
+	var data = []
+	for (var i = 0; i < 15; i ++) {
+		data.push({
+			id: String(from + i),
+			text: String(from + i) + '年'
+		})
+	}
+	return data
+}
+
+function getDays() {
   var data = []
-  for (var i = 0; i < 15; i ++) {
+  for (var i = 1; i <= 31; i ++) {
     data.push({
-      id: String(from + i),
-      text: String(from + i) + '年'
+      id: String(i),
+      text: String(i)
     })
   }
   return data
 }
 
-document.getElementById('prev').addEventListener('click', function () {
-  select.prev()
-}, false)
+function getMonths() {
+	var data = []
+	for (var i = 1; i <= 12; i ++) {
+		data.push({
+			id: String(i),
+			text: String(i)
+		})
+	}
+	return data
+}
 
-document.getElementById('next').addEventListener('click', function () {
-  select.next()
-}, false)
-
-document.getElementById('rebuild').addEventListener('click', function () {
-  select.setData(getData(1970))
-}, false)
-
-document.getElementById('unbind').addEventListener('click', function () {
-  select.unbind()
-}, false)
+// document.getElementById('prev').addEventListener('click', function () {
+//   select.prev()
+// }, false)
+//
+// document.getElementById('next').addEventListener('click', function () {
+//   select.next()
+// }, false)
+//
+// document.getElementById('rebuild').addEventListener('click', function () {
+//   select.setData(getData(1970))
+// }, false)
+//
+// document.getElementById('unbind').addEventListener('click', function () {
+//   select.unbind()
+// }, false)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var Emitter = require('emitter')
-var domify = require('domify')
-var _ = require('dom')
-var Iscroll = require('iscroll')
-var hasTouch = require('has-touch')
+var Emitter = require('emitter');
+var domify = require('domify');
+var _ = require('dom');
+var Iscroll = require('iscroll');
+var hasTouch = require('has-touch');
+var TouchEmulator = require('hammer-touchemulator')();
 
 /**
  * Init select with `el` and optional option
@@ -13,24 +14,26 @@ var hasTouch = require('has-touch')
  * @returns {undefined}
  */
 function Select(el, opt) {
-  if (!(this instanceof Select)) return new Select(el)
-  var container = this.container = create('div', 'scroll-select-container')
-  el.appendChild(this.container)
-  this.rowHeight = opt.rowHeight || 30
-  var data = opt.data || []
-  var main = create('div')
-  container.appendChild(main)
-  container.appendChild(create('div', 'scroll-select-top'))
-  container.appendChild(create('div', 'scroll-select-bottom'))
+  if (!(this instanceof Select)) {
+  	return new Select(el);
+  }
+  var container = this.container = create('div', 'scroll-select-container');
+  el.appendChild(this.container);
+  this.rowHeight = opt.rowHeight || 30;
+  var data = opt.data || [];
+  var main = create('div');
+  container.appendChild(main);
+  container.appendChild(create('div', 'scroll-select-top'));
+  container.appendChild(create('div', 'scroll-select-bottom'));
   var iscroll = this.iscroll = new Iscroll(container);
-  this.el = create('ul', 'scroll-select')
-  main.appendChild(this.el)
-  this.setData(data)
-  this._onscrollend = this.onscrollend.bind(this)
-  iscroll.on('scrollend', this._onscrollend)
+  this.el = create('ul', 'scroll-select');
+  main.appendChild(this.el);
+  this.setData(data);
+  this._onscrollend = this.onscrollend.bind(this);
+  iscroll.on('scrollend', this._onscrollend);
 }
 
-Emitter(Select.prototype)
+Emitter(Select.prototype);
 
 /**
  * Set internal data
@@ -39,28 +42,29 @@ Emitter(Select.prototype)
  * @param {Array} data
  */
 Select.prototype.setData = function (data) {
-  var v = this.value()
-  var parentNode = this.el
-  var is = this.iscroll
-  this.data = data
-  _(parentNode).clean('.scroll-select-item')
-  var fragment = document.createDocumentFragment()
+  var v = this.value();
+  var parentNode = this.el;
+  var is = this.iscroll;
+  this.data = data;
+  _(parentNode).clean('.scroll-select-item');
+  var fragment = document.createDocumentFragment();
   for (var i = 0, l = data.length; i < l; i++) {
-    var o = data[i]
-    var el = domify('<li class="scroll-select-item" data-index="' +
-         i + '">' + o.text + '</li>')
-    fragment.appendChild(el)
+    var o = data[i];
+    var el = domify('<li class="scroll-select-item" data-index="' + i + '" data-value="' + o.text + '">' + o.text + '</li>');
+    fragment.appendChild(el);
   }
-  parentNode.appendChild(fragment)
+  parentNode.appendChild(fragment);
   //no scroll refresh
-  is.refresh(true)
+  is.refresh(true);
   var vals = data.map(function (o) {
-    return String(o.id)
-  })
-  var idx = vals.indexOf(v)
-  if (v == null || idx === -1) return is.scrollTo(0, 0)
-  is.scrollTo(- idx*this.rowHeight, 0)
-}
+    return String(o.id);
+  });
+  var idx = vals.indexOf(v);
+  if (v == null || idx === -1) {
+  	return is.scrollTo(0, 0);
+  }
+  is.scrollTo(- idx * this.rowHeight, 0);
+};
 
 /**
  * Unbind all event listeners
@@ -68,10 +72,10 @@ Select.prototype.setData = function (data) {
  * @public
  */
 Select.prototype.unbind = function () {
-  this.iscroll.unbind()
-  this.off()
-  _(this.container).remove()
-}
+  this.iscroll.unbind();
+  this.off();
+  _(this.container).remove();
+};
 
 /**
  * Select previous item
@@ -79,10 +83,10 @@ Select.prototype.unbind = function () {
  * @public
  */
 Select.prototype.prev = function () {
-  var is = this.iscroll
-  var y = is.y
-  is.scrollTo(y + this.rowHeight, 350)
-}
+  var is = this.iscroll;
+  var y = is.y;
+  is.scrollTo(y + this.rowHeight, 350);
+};
 
 /**
  * Select next item
@@ -90,10 +94,10 @@ Select.prototype.prev = function () {
  * @public
  */
 Select.prototype.next = function () {
-  var is = this.iscroll
-  var y = is.y
-  is.scrollTo(y - this.rowHeight, 350)
-}
+  var is = this.iscroll;
+  var y = is.y;
+  is.scrollTo(y - this.rowHeight, 350);
+};
 
 /**
  * Scroll end event handler
@@ -101,23 +105,27 @@ Select.prototype.next = function () {
  * @private
  */
 Select.prototype.onscrollend = function () {
-  var is = this.iscroll
-  var y = is.y
-  if (y > 0) return is.scrollTo(0, 300)
-  if (y < is.minY) return is.scrollTo(is.minY, 300)
-  if (y%this.rowHeight === 0) {
-    var v = this.value()
-    if (this._value !== v) {
-      this.emit('change', v, this._value)
-    }
-    this._value = v
-    return
+  var is = this.iscroll;
+  var y = is.y;
+  if (y > 0) {
+  	return is.scrollTo(0, 300);
   }
-  var m = is.direction == 1 ? Math.floor : Math.ceil
-  m = hasTouch? Math.round : m
-  var dest = this.rowHeight * m(y/this.rowHeight)
-  is.scrollTo(dest, 300)
-}
+  if (y < is.minY) {
+  	return is.scrollTo(is.minY, 300);
+  }
+  if (y % this.rowHeight === 0) {
+    var v = this.value();
+    if (this._value !== v) {
+      this.emit('change', v, this._value);
+    }
+    this._value = v;
+    return;
+  }
+  var m = is.direction == 1 ? Math.floor : Math.ceil;
+  m = hasTouch ? Math.round : m;
+  var dest = this.rowHeight * m(y / this.rowHeight);
+  is.scrollTo(dest, 300);
+};
 
 /**
  * Get/Set the value
@@ -127,24 +135,51 @@ Select.prototype.onscrollend = function () {
  */
 Select.prototype.value = function (value) {
   if (arguments.length === 0) {
-    if (!this.data) return
-    var n = Math.round(- this.iscroll.y/this.rowHeight)
-    return this.data[n].id
+    if (!this.data) {
+    	return;
+	}
+    var n = Math.round(- this.iscroll.y / this.rowHeight);
+    return this.data[n].id;
   }
   for (var i = 0, l = this.data.length; i < l; i++) {
-    var o = this.data[i]
+    var o = this.data[i];
     if (o.id == value) {
-      this.iscroll.scrollTo(- i*this.rowHeight, 0)
-      return
+      this.iscroll.scrollTo(- i * this.rowHeight, 0);
+      return;
     }
   }
-  throw new Error('value: ' + value + ' not found on data')
-}
+  throw new Error('value: ' + value + ' not found on data');
+};
+
+/**
+ * Hides values from select
+ *
+ * @public
+ * @param {Array|String} values
+ */
+Select.prototype.filter = function (values) {
+	if (arguments.length === 0) {
+		var _items = document.querySelectorAll('.scroll-select-item');
+		for (var i = 0; i < _items.length; i++) {
+			_items[i].style.display = 'block';
+		}
+		return;
+	}
+	if (typeof values === 'object') {
+		for (var i = 0; i < values.length; i++) {
+			document.querySelector('.scroll-select-item[data-value="' + values[i] + '"]').style.display = 'none';
+		}
+	} else if (typeof values === 'string') {
+		document.querySelector('.scroll-select-item[data-value="' + values + '"]').style.display = 'none';
+	}
+};
 
 function create(tag, className) {
-  var el = document.createElement(tag)
-  if (className) el.className = className
-  return el
+  var el = document.createElement(tag);
+  if (className) {
+  	el.className = className;
+  }
+  return el;
 }
 
-module.exports = Select
+module.exports = Select;
